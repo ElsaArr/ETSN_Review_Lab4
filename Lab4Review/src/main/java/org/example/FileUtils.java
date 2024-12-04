7ce7fe1333eb02dd533f24d1dcfdb8f3ee36a7dd
ElsaArr <elsa.arrive@eleves.ec-nantes.fr>
2024-12-04 10:34:15 +0100

    Base version works

--- Lab4Review/src/main/java/org/example/FileUtils.java
+++ Lab4Review/src/main/java/org/example/FileUtils.java
@@ -1,2 +1,28 @@
-package org.example;public class FileUtils {
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+public class FileUtils {
+    public static void searchStringInFile(String pattern, String path) throws FileNotFoundException {
+        String line;
+        try {
+            FileReader fileReader= new FileReader(path);
+            BufferedReader file=new BufferedReader(fileReader);
+            line = file.readLine();
+            while (line!=null){
+                System.out.println(line);
+                if (line.contains(pattern)){
+                    System.out.println("Pattern found in line: "+line);
+                }
+                line=file.readLine();
+            }
+            file.close();
+        } catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- Lab4Review/src/main/java/org/example/Main.java
+++ Lab4Review/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String []args)
+    {
+        String path = "C:\\Users\\elsaa\\Documents\\Centrale\\LTH\\ETSN20\\Labs\\Lab4\\testTexte1.txt";
+        String searchWord = "Type";
+
+        try {
+            FileUtils.searchStringInFile(searchWord, path) ;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
